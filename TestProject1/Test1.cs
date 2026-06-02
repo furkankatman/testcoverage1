69135e329ed6fb96e74096e38bd24d7466cae068
KATMAN, FURKAN Sertap <FURKANSertap.KATMAN@bakerhughes.com>
2026-06-02 15:49:31 +0200

    added test project

---
+++ TestProject1/Test1.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssert;
+using testcoverage1;
+
+
+namespace TestProject1
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void CarChangesGear_onGearUp()
+        {
+            Car car1 = new Car();
+            car1.GearUp();
+            car1.ShowStateAsString().ToLower().ShouldContain("gear: 1");
+        }
+    }
+}
