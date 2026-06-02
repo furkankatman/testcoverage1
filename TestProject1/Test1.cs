2556e8c583975a16ebe39eca5776913e56ff1fcb
KATMAN, FURKAN Sertap <FURKANSertap.KATMAN@bakerhughes.com>
2026-06-02 17:16:41 +0200

    added one more case for geardown

--- TestProject1/Test1.cs
+++ TestProject1/Test1.cs
@@ -14,5 +14,14 @@ namespace TestProject1
             car1.GearUp();
             StringAssert.Contains(car1.ShowStateAsString(), "Gear: 1");
         }
+
+        [TestMethod]
+        public void CarChangesGear_onGearDown()
+        {
+            Car car1 = new Car();
+            car1.GearUp();
+            car1.GearDown();
+            StringAssert.Contains(car1.ShowStateAsString(), "Gear: 0");
+        }
     }
 }
